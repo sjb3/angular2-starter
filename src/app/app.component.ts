250dd73eac535d8b6797e6f368316bdf40f648fc
Sung J Byun <sungjbyun@D-69-91-132-248.dhcp4.washington.edu>
2017-04-11 15:28:16 -0700

    add app.module.ts-updated version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,1 +1,12 @@
-import {Component} 
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My App</h1>',
+})
+
+export class AppComponent {}
+
+
+
+
